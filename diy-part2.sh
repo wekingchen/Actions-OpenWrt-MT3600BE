43d24cb7ddebb67cc9e771ff51994698ccdc4526
wekingchen <weking.chen@gmail.com>
2026-05-27 15:43:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,28 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 移除 openwrt feeds 自带的核心包
+rm -rf feeds/packages/net/{xray-core,xray-plugin,v2ray-core,v2ray-plugin,v2ray-geodata,sing-box,hysteria,naiveproxy,shadowsocks-rust,shadow-tls,tuic-client,microsocks,chinadns-ng,dns2socks,ipt2socks}
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus}
+cp -r feeds/passwall_packages/{xray-core,xray-plugin,v2ray-plugin,v2ray-geodata,sing-box,hysteria,naiveproxy,shadowsocks-rust,shadow-tls,tuic-client,microsocks,chinadns-ng,dns2socks,ipt2socks} feeds/packages/net/
+cp -r feeds/helloworld/v2ray-core feeds/packages/net/
+cp -r feeds/passwall/luci-app-passwall feeds/luci/applications
+cp -r feeds/helloworld/luci-app-ssr-plus feeds/luci/applications
+
+# 修改golang源码以编译xray1.8.8+版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+# 修改frp版本为官网最新v0.68.1 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
+rm -rf feeds/packages/net/frp
+wget https://github.com/coolsnowwolf/packages/archive/0f7be9fc93d68986c179829d8199824d3183eb60.zip -O OldPackages.zip
+unzip OldPackages.zip
+cp -r packages-0f7be9fc93d68986c179829d8199824d3183eb60/net/frp feeds/packages/net/
+rm -rf OldPackages.zip packages-0f7be9fc93d68986c179829d8199824d3183eb60
+sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.68.1/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=44ed7107bf35e4f68dc0e77cd5805102effa5301528b89ee5ab0ab379088edc6/' feeds/packages/net/frp/Makefile
+
+# 升级zerotier到官方最新版本1.14.2
+sed -i 's/PKG_VERSION:=1.14.1/PKG_VERSION:=1.14.2/' feeds/packages/net/zerotier/Makefile
+sed -i 's/PKG_HASH:=4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/PKG_HASH:=c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/' feeds/packages/net/zerotier/Makefile
